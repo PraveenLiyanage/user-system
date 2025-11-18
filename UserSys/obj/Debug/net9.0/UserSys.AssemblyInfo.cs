6218f7db7089f0fbf30330d831b25c61e54159fc
Praveen Liyanage <111709030+PraveenLiyanage@users.noreply.github.com>
2025-11-18 14:24:16 +0530

    pubspecs update

--- UserSys/obj/Debug/net9.0/UserSys.AssemblyInfo.cs
+++ UserSys/obj/Debug/net9.0/UserSys.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserSys")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c616f76f31c5f382486ab61d203472ff569a48c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3696a8492aaa1ca354642ca9c19c48c05abc5a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserSys")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserSys")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
